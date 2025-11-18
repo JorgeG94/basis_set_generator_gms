410e27cfb3316e21caa7aa39b833d61ba8c61180
JorgeGV <jorgegalvez1694@gmail.com>
2025-11-18 11:02:15 +1100

    fomratting

--- basis_parser/app/main.f90
+++ basis_parser/app/main.f90
@@ -1,23 +1,23 @@
 program main
   use gms_basis_reader, only: say_hello, classify_line, parse_element_basis, &
-  build_molecular_basis
-  use  gms_cgto
+                              build_molecular_basis
+  use gms_cgto
   use iso_fortran_env, only: real64
   implicit none
-      character(len=*), parameter :: test_basis = &
-        "$DATA" // new_line('a') // &
-        "" // new_line('a') // &
-        "HYDROGEN" // new_line('a') // &
-        "S 2" // new_line('a') // &
-        "1 1.0 2.0" // new_line('a') // &
-        "2 1.0 2.0" // new_line('a') // &
-        "" // new_line('a') // &
-        "L 2" // new_line('a') // &
-        "1 1.0 2.0 3.0" // new_line('a') // &
-        "2 4.0 5.0 6.0" // new_line('a') // &
-        "" // new_line('a') // &
-        "$END"
-  
+  character(len=*), parameter :: test_basis = &
+                                 "$DATA"//new_line('a')// &
+                                 ""//new_line('a')// &
+                                 "HYDROGEN"//new_line('a')// &
+                                 "S 2"//new_line('a')// &
+                                 "1 1.0 2.0"//new_line('a')// &
+                                 "2 1.0 2.0"//new_line('a')// &
+                                 ""//new_line('a')// &
+                                 "L 2"//new_line('a')// &
+                                 "1 1.0 2.0 3.0"//new_line('a')// &
+                                 "2 4.0 5.0 6.0"//new_line('a')// &
+                                 ""//new_line('a')// &
+                                 "$END"
+
   print *, test_basis
 
   call test_classify_lines()
@@ -28,77 +28,77 @@ program main
 
 contains
 
-    subroutine test_classify_lines()
-        integer :: line_start, line_end, line_num
-        character(len=256) :: line
-        integer :: line_type
+  subroutine test_classify_lines()
+    integer :: line_start, line_end, line_num
+    character(len=256) :: line
+    integer :: line_type
 
-        print *, "Testing line classification:"
-        print *, "============================"
+    print *, "Testing line classification:"
+    print *, "============================"
 
-        line_start = 1
-        line_num = 0
+    line_start = 1
+    line_num = 0
 
-        do while (line_start <= len(test_basis))
-            ! Find next newline
-            line_end = index(test_basis(line_start:), new_line('a'))
-            if (line_end == 0) then
-                line = test_basis(line_start:)
-                line_start = len(test_basis) + 1
-            else
-                line = test_basis(line_start:line_start+line_end-2)
-                line_start = line_start + line_end
-            end if
+    do while (line_start <= len(test_basis))
+      ! Find next newline
+      line_end = index(test_basis(line_start:), new_line('a'))
+      if (line_end == 0) then
+        line = test_basis(line_start:)
+        line_start = len(test_basis) + 1
+      else
+        line = test_basis(line_start:line_start + line_end - 2)
+        line_start = line_start + line_end
+      end if
 
-            line_num = line_num + 1
-            line = adjustl(line)
+      line_num = line_num + 1
+      line = adjustl(line)
 
-            if (len_trim(line) == 0) cycle
+      if (len_trim(line) == 0) cycle
 
-            line_type = classify_line(line)
+      line_type = classify_line(line)
 
-            print '(i3,a,i2,a,a)', line_num, ': Type=', line_type, ' | ', trim(line)
-        end do
+      print '(i3,a,i2,a,a)', line_num, ': Type=', line_type, ' | ', trim(line)
+    end do
 
-    end subroutine test_classify_lines
+  end subroutine test_classify_lines
 
-    subroutine test_parse_element()
+  subroutine test_parse_element()
     type(atomic_basis_type) :: h_basis
     integer :: stat, i, j
     character(len=:), allocatable :: errmsg
     character(len=1), dimension(0:6) :: ang_mom_names = ['S', 'P', 'D', 'F', 'G', 'H', 'I']
-    
+
     print *, "Testing parse_element_basis:"
     print *, "============================"
     print *
-    
+
     call parse_element_basis(test_basis, "HYDROGEN", h_basis, stat, errmsg)
-    
+
     if (stat /= 0) then
-        print *, "ERROR: ", errmsg
-        return
+      print *, "ERROR: ", errmsg
+      return
     end if
-    
+
     print *, "Successfully parsed basis for: ", h_basis%element
     print *, "Number of shells: ", h_basis%nshells
     print *
-    
+
     ! Print each shell
     do i = 1, h_basis%nshells
-        print '(a,i0,a,a,a,i0,a)', "Shell ", i, " (", ang_mom_names(h_basis%shells(i)%l), &
-            "): ", h_basis%shells(i)%nfunc, " primitives"
-        do j = 1, h_basis%shells(i)%nfunc
-            print '(2x,i2,2x,f12.6,2x,f12.6)', j, h_basis%shells(i)%exponents(j), &
-                h_basis%shells(i)%coefficients(j)
-        end do
-        print *
+      print '(a,i0,a,a,a,i0,a)', "Shell ", i, " (", ang_mom_names(h_basis%shells(i)%l), &
+        "): ", h_basis%shells(i)%nfunc, " primitives"
+      do j = 1, h_basis%shells(i)%nfunc
+        print '(2x,i2,2x,f12.6,2x,f12.6)', j, h_basis%shells(i)%exponents(j), &
+          h_basis%shells(i)%coefficients(j)
+      end do
+      print *
     end do
-    
-    call h_basis%destroy()
-    
-end subroutine test_parse_element
 
-subroutine test_h2_molecule()
+    call h_basis%destroy()
+
+  end subroutine test_parse_element
+
+  subroutine test_h2_molecule()
     type(molecular_basis_type) :: h2_basis
     integer :: stat, iatom, ishell, ifunc
     character(len=:), allocatable :: errmsg
@@ -112,8 +112,8 @@ subroutine test_h2_molecule()
     call build_molecular_basis(test_basis, h2_atoms, h2_basis, stat, errmsg)
 
     if (stat /= 0) then
-        print *, "ERROR: ", errmsg
-        return
+      print *, "ERROR: ", errmsg
+      return
     end if
 
     print *, "Successfully built molecular basis for H2"
@@ -123,25 +123,25 @@ subroutine test_h2_molecule()
 
     ! Print basis for each atom
     do iatom = 1, h2_basis%nelements
-        print '(a,i0,a,a)', "Atom ", iatom, ": ", trim(h2_basis%elements(iatom)%element)
-        print '(a,i0,a)', "  Number of shells: ", h2_basis%elements(iatom)%nshells
+      print '(a,i0,a,a)', "Atom ", iatom, ": ", trim(h2_basis%elements(iatom)%element)
+      print '(a,i0,a)', "  Number of shells: ", h2_basis%elements(iatom)%nshells
 
-        do ishell = 1, h2_basis%elements(iatom)%nshells
-            print '(a,i0,a,a,a,i0,a)', "  Shell ", ishell, " (", &
-                ang_mom_names(h2_basis%elements(iatom)%shells(ishell)%l), &
-                "): ", h2_basis%elements(iatom)%shells(ishell)%nfunc, " primitives"
+      do ishell = 1, h2_basis%elements(iatom)%nshells
+        print '(a,i0,a,a,a,i0,a)', "  Shell ", ishell, " (", &
+          ang_mom_names(h2_basis%elements(iatom)%shells(ishell)%l), &
+          "): ", h2_basis%elements(iatom)%shells(ishell)%nfunc, " primitives"
 
-            do ifunc = 1, h2_basis%elements(iatom)%shells(ishell)%nfunc
-                print '(4x,i2,2x,f12.6,2x,f12.6)', ifunc, &
-                    h2_basis%elements(iatom)%shells(ishell)%exponents(ifunc), &
-                    h2_basis%elements(iatom)%shells(ishell)%coefficients(ifunc)
-            end do
+        do ifunc = 1, h2_basis%elements(iatom)%shells(ishell)%nfunc
+          print '(4x,i2,2x,f12.6,2x,f12.6)', ifunc, &
+            h2_basis%elements(iatom)%shells(ishell)%exponents(ifunc), &
+            h2_basis%elements(iatom)%shells(ishell)%coefficients(ifunc)
         end do
-        print *
+      end do
+      print *
     end do
 
     call h2_basis%destroy()
 
-end subroutine test_h2_molecule
+  end subroutine test_h2_molecule
 
 end program main
--- basis_parser/src/gms_cgto.f90
+++ basis_parser/src/gms_cgto.f90
@@ -1,33 +1,33 @@
 module gms_cgto
-use iso_fortran_env, only: real64
-implicit none 
-private 
+  use iso_fortran_env, only: real64
+  implicit none
+  private
 
-public :: cgto_type, atomic_basis_type, molecular_basis_type
+  public :: cgto_type, atomic_basis_type, molecular_basis_type
 
   type :: cgto_type
-      integer :: l                                    
+    integer :: l
         !! Angular momentum quantum number (0=s, 1=p, 2=d, etc.)
-      integer :: nfunc                                
+    integer :: nfunc
         !! Number of primitive Gaussians in the contraction
-      real(real64), allocatable :: exponents(:)       
+    real(real64), allocatable :: exponents(:)
         !! Exponents (alpha values)
-      real(real64), allocatable :: coefficients(:)    
+    real(real64), allocatable :: coefficients(:)
         !! Contraction coefficients
   contains
-      procedure :: allocate_arrays => cgto_allocate_arrays
-      procedure :: destroy => cgto_destroy
-      procedure :: num_basis_functions => cgto_num_basis_functions
+    procedure :: allocate_arrays => cgto_allocate_arrays
+    procedure :: destroy => cgto_destroy
+    procedure :: num_basis_functions => cgto_num_basis_functions
   end type cgto_type
 
   type :: atomic_basis_type
-    character(len=:), allocatable :: element 
+    character(len=:), allocatable :: element
       !! element symbol
-    type(cgto_type), allocatable :: shells(:) 
+    type(cgto_type), allocatable :: shells(:)
       !! array of contracted shells
-    integer :: nshells 
+    integer :: nshells
       !! number of shells in type
-  contains 
+  contains
     procedure :: allocate_shells => allocate_basis_shells
     procedure :: destroy => atomic_basis_destroy
     procedure :: num_basis_functions => atomic_basis_num_basis_functions
@@ -38,110 +38,108 @@ public :: cgto_type, atomic_basis_type, molecular_basis_type
       !! array of atomic basis types
     integer :: nelements
       !! total number of atoms/elements in a molecule
-  contains 
+  contains
     procedure :: allocate_elements => basis_set_allocate_elements
     procedure :: destroy => basis_set_destroy
     procedure :: num_basis_functions => molecular_basis_num_basis_functions
   end type molecular_basis_type
 
-contains 
+contains
 
- pure subroutine cgto_allocate_arrays(self, nfunc)
-   class(cgto_type), intent(inout) :: self 
-   integer, intent(in) :: nfunc
+  pure subroutine cgto_allocate_arrays(self, nfunc)
+    class(cgto_type), intent(inout) :: self
+    integer, intent(in) :: nfunc
 
-   self%nfunc = nfunc 
-   allocate(self%exponents(nfunc))
-   allocate(self%coefficients(nfunc))
- end subroutine cgto_allocate_arrays
+    self%nfunc = nfunc
+    allocate (self%exponents(nfunc))
+    allocate (self%coefficients(nfunc))
+  end subroutine cgto_allocate_arrays
 
- pure subroutine cgto_destroy(self)
-  class(cgto_type), intent(inout) :: self 
+  pure subroutine cgto_destroy(self)
+    class(cgto_type), intent(inout) :: self
 
-  if(allocated(self%exponents)) deallocate(self%exponents)
-  if(allocated(self%coefficients)) deallocate(self%coefficients)
-  self%nfunc = 0
-  self%l = 0 
- end subroutine cgto_destroy
+    if (allocated(self%exponents)) deallocate (self%exponents)
+    if (allocated(self%coefficients)) deallocate (self%coefficients)
+    self%nfunc = 0
+    self%l = 0
+  end subroutine cgto_destroy
 
-
- pure subroutine allocate_basis_shells(self, nshells)
+  pure subroutine allocate_basis_shells(self, nshells)
     class(atomic_basis_type), intent(inout) :: self
-    integer, intent(in) :: nshells 
+    integer, intent(in) :: nshells
 
-    self%nshells = nshells 
-    allocate(self%shells(nshells))
- end subroutine allocate_basis_shells
+    self%nshells = nshells
+    allocate (self%shells(nshells))
+  end subroutine allocate_basis_shells
 
- pure subroutine atomic_basis_destroy(self)
-   class(atomic_basis_type), intent(inout) :: self 
-   integer :: i 
+  pure subroutine atomic_basis_destroy(self)
+    class(atomic_basis_type), intent(inout) :: self
+    integer :: i
 
-   if (allocated(self%shells)) then 
-    do i = 1, self%nshells 
-      call self%shells(i)%destroy()
-    end do
-    deallocate(self%shells)
-  end if
-  if (allocated(self%element)) deallocate(self%element)
-  self%nshells = 0
-end subroutine atomic_basis_destroy
+    if (allocated(self%shells)) then
+      do i = 1, self%nshells
+        call self%shells(i)%destroy()
+      end do
+      deallocate (self%shells)
+    end if
+    if (allocated(self%element)) deallocate (self%element)
+    self%nshells = 0
+  end subroutine atomic_basis_destroy
 
+  pure subroutine basis_set_allocate_elements(self, nelements)
+    class(molecular_basis_type), intent(inout) :: self
+    integer, intent(in) :: nelements
 
-pure subroutine basis_set_allocate_elements(self, nelements)
-  class(molecular_basis_type), intent(inout) :: self
-  integer, intent(in) :: nelements
+    self%nelements = nelements
+    allocate (self%elements(nelements))
 
-  self%nelements = nelements 
-  allocate(self%elements(nelements))
+  end subroutine basis_set_allocate_elements
 
-end subroutine basis_set_allocate_elements 
+  pure subroutine basis_set_destroy(self)
+    class(molecular_basis_type), intent(inout) :: self
+    integer :: i
 
-pure subroutine basis_set_destroy(self)
-  class(molecular_basis_type), intent(inout) :: self 
-  integer :: i 
+    if (allocated(self%elements)) then
+      do i = 1, self%nelements
+        call self%elements(i)%destroy()
+      end do
+      deallocate (self%elements)
+    end if
 
-  if(allocated(self%elements)) then 
-    do i = 1, self%nelements 
-      call self%elements(i)%destroy()
-    end do 
-    deallocate(self%elements)
-  end if
-
-  self%nelements = 0 
-end subroutine basis_set_destroy
+    self%nelements = 0
+  end subroutine basis_set_destroy
 
 !> Get number of basis functions in a shell (Cartesian)
-pure function cgto_num_basis_functions(self) result(nbf)
+  pure function cgto_num_basis_functions(self) result(nbf)
     class(cgto_type), intent(in) :: self
     integer :: nbf
 
     ! Cartesian: (l+1)*(l+2)/2
-    nbf = (self%l + 1) * (self%l + 2) / 2
-end function cgto_num_basis_functions
+    nbf = (self%l + 1)*(self%l + 2)/2
+  end function cgto_num_basis_functions
 
 !> Get total number of basis functions for an atom
-pure function atomic_basis_num_basis_functions(self) result(nbf)
+  pure function atomic_basis_num_basis_functions(self) result(nbf)
     class(atomic_basis_type), intent(in) :: self
     integer :: nbf
     integer :: ishell
 
     nbf = 0
     do ishell = 1, self%nshells
-        nbf = nbf + self%shells(ishell)%num_basis_functions()
+      nbf = nbf + self%shells(ishell)%num_basis_functions()
     end do
-end function atomic_basis_num_basis_functions
+  end function atomic_basis_num_basis_functions
 
 !> Get total number of basis functions for the molecule
-pure function molecular_basis_num_basis_functions(self) result(nbf)
+  pure function molecular_basis_num_basis_functions(self) result(nbf)
     class(molecular_basis_type), intent(in) :: self
     integer :: nbf
     integer :: iatom
 
     nbf = 0
     do iatom = 1, self%nelements
-        nbf = nbf + self%elements(iatom)%num_basis_functions()
+      nbf = nbf + self%elements(iatom)%num_basis_functions()
     end do
-end function molecular_basis_num_basis_functions
+  end function molecular_basis_num_basis_functions
 
 end module gms_cgto
