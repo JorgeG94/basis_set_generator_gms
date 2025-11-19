061f8864bfcb9cf074edb039022ca070b1cc9a61
JorgeGV <jorgegalvez1694@gmail.com>
2025-11-19 21:54:20 +1100

    refactoring

--- basis_parser/src/gms_xyz_reader.f90
+++ basis_parser/src/gms_xyz_reader.f90
@@ -2,9 +2,13 @@ module gms_xyz_reader
   use iso_fortran_env, only: real64
   implicit none
   private
-  
+
   public :: geometry_type
   public :: read_xyz_file, read_xyz_string
+
+  ! Parameters
+  integer, parameter :: MAX_ELEMENT_SYMBOL_LEN = 4
+  integer, parameter :: MAX_ATOMS = 1000000  ! Reasonable upper limit
   
   type :: geometry_type
     integer :: natoms
@@ -68,12 +72,11 @@ subroutine read_xyz_string(xyz_string, geom, stat, errmsg)
   type(geometry_type), intent(out) :: geom
   integer, intent(out) :: stat
   character(len=:), allocatable, intent(out) :: errmsg
-  
+
   character(len=:), allocatable :: lines(:)
-  integer :: nlines, iatom, io_stat, max_element_len
+  integer :: nlines, iatom, io_stat
   character(len=256) :: element
   real(real64) :: x, y, z
-  character(len=500) :: temp_msg
   
   stat = 0
   
@@ -99,22 +102,28 @@ subroutine read_xyz_string(xyz_string, geom, stat, errmsg)
     errmsg = "Number of atoms must be non-negative"
     return
   end if
-  
+
+  if (geom%natoms > MAX_ATOMS) then
+    stat = 1
+    errmsg = "Number of atoms exceeds maximum allowed (" // &
+             trim(int_to_string(MAX_ATOMS)) // ")"
+    return
+  end if
+
   ! Store comment line
   geom%comment = trim(adjustl(lines(2)))
-  
+
   ! Check we have enough lines
   if (nlines < 2 + geom%natoms) then
     stat = 1
-    write(temp_msg, '(A,I0,A,I0,A)') "XYZ file has insufficient lines: expected ", &
-                                      2 + geom%natoms, ", got ", nlines, ""
-    errmsg = trim(temp_msg)
+    errmsg = "XYZ file has insufficient lines: expected " // &
+             trim(int_to_string(2 + geom%natoms)) // ", got " // &
+             trim(int_to_string(nlines))
     return
   end if
-  
+
   ! Allocate arrays
-  max_element_len = 4  ! Enough for most element symbols
-  allocate(character(len=max_element_len) :: geom%elements(geom%natoms))
+  allocate(character(len=MAX_ELEMENT_SYMBOL_LEN) :: geom%elements(geom%natoms))
   allocate(geom%coords(3, geom%natoms))
   
   ! Read atom data
@@ -122,100 +131,134 @@ subroutine read_xyz_string(xyz_string, geom, stat, errmsg)
     read(lines(2 + iatom), *, iostat=io_stat) element, x, y, z
     if (io_stat /= 0) then
       stat = 1
-      write(temp_msg, '(A,I0,A)') "Failed to parse atom data on line ", &
-                                   2 + iatom, ": '"//trim(lines(2 + iatom))//"'"
-      errmsg = trim(temp_msg)
+      errmsg = "Failed to parse atom data on line " // &
+               trim(int_to_string(2 + iatom)) // ": '" // &
+               trim(lines(2 + iatom)) // "'"
       return
     end if
-    
+
     geom%elements(iatom) = trim(adjustl(element))
     geom%coords(1, iatom) = x
     geom%coords(2, iatom) = y
     geom%coords(3, iatom) = z
   end do
-  
+
 end subroutine read_xyz_string
 
+!> Helper function to convert integer to string
+function int_to_string(i) result(str)
+  integer, intent(in) :: i
+  character(len=:), allocatable :: str
+  character(len=20) :: buffer
+
+  write(buffer, '(I0)') i
+  str = trim(adjustl(buffer))
+end function int_to_string
+
+!> Split text into lines handling LF, CRLF, and CR line endings
+!! Trailing newlines do not create empty lines
 subroutine split_lines(text, lines, nlines)
   character(len=*), intent(in) :: text
   character(len=:), allocatable, intent(out) :: lines(:)
   integer, intent(out) :: nlines
-  
-  integer :: i, line_start, max_line_len
-  character(len=1) :: ch
+
+  integer :: i, line_start, line_end, max_line_len
   character(len=:), allocatable :: temp_lines(:)
-  
-  ! Count lines and find max length
-  nlines = 1  ! Start with 1 since we always have at least one line
-  line_start = 1
+
+  if (len(text) == 0) then
+    nlines = 0
+    allocate(character(len=1) :: lines(0))
+    return
+  end if
+
+  ! Pass 1: Count lines and find maximum line length
+  nlines = 0
   max_line_len = 0
-  
-  do i = 1, len(text)
-    ch = text(i:i)
-    if (ch == new_line('a') .or. ch == achar(10)) then
-      max_line_len = max(max_line_len, i - line_start)
-      nlines = nlines + 1
-      line_start = i + 1
-    else if (ch == achar(13)) then  ! CR - check for CRLF
-      if (i < len(text)) then
-        if (text(i+1:i+1) == achar(10)) cycle  ! Skip CR in CRLF
+  line_start = 1
+  i = 1
+
+  do while (i <= len(text))
+    ! Check for line ending
+    if (text(i:i) == achar(13)) then  ! CR
+      ! Check for CRLF
+      if (i < len(text) .and. text(i+1:i+1) == achar(10)) then
+        line_end = i - 1
+        i = i + 2  ! Skip both CR and LF
+      else
+        line_end = i - 1
+        i = i + 1
       end if
-      max_line_len = max(max_line_len, i - line_start)
       nlines = nlines + 1
-      line_start = i + 1
+      max_line_len = max(max_line_len, line_end - line_start + 1)
+      line_start = i
+    else if (text(i:i) == achar(10)) then  ! LF
+      line_end = i - 1
+      nlines = nlines + 1
+      max_line_len = max(max_line_len, line_end - line_start + 1)
+      i = i + 1
+      line_start = i
+    else
+      i = i + 1
     end if
   end do
-  
-  ! Handle last line
-  max_line_len = max(max_line_len, len(text) - line_start + 1)
-  
-  ! Need at least length 1 for empty lines
+
+  ! Handle last line if text doesn't end with newline
+  if (line_start <= len(text)) then
+    nlines = nlines + 1
+    max_line_len = max(max_line_len, len(text) - line_start + 1)
+  end if
+
+  ! Handle empty text or ensure at least length 1
   if (max_line_len == 0) max_line_len = 1
-  
-  ! Allocate output with explicit character length
+
+  ! Allocate output array
   allocate(character(len=max_line_len) :: temp_lines(nlines))
   temp_lines = ""  ! Initialize all to empty
-  
-  ! Extract lines
+
+  ! Pass 2: Extract lines
   nlines = 0
   line_start = 1
-  
-  do i = 1, len(text)
-    ch = text(i:i)
-    if (ch == new_line('a') .or. ch == achar(10)) then
-      nlines = nlines + 1
-      if (i > line_start) then
-        temp_lines(nlines) = text(line_start:i-1)
+  i = 1
+
+  do while (i <= len(text))
+    ! Check for line ending
+    if (text(i:i) == achar(13)) then  ! CR
+      ! Check for CRLF
+      if (i < len(text) .and. text(i+1:i+1) == achar(10)) then
+        line_end = i - 1
+        i = i + 2
       else
-        temp_lines(nlines) = ""  ! Empty line
-      end if
-      line_start = i + 1
-    else if (ch == achar(13)) then  ! CR
-      if (i < len(text)) then
-        if (text(i+1:i+1) == achar(10)) cycle  ! Skip CR in CRLF
+        line_end = i - 1
+        i = i + 1
       end if
       nlines = nlines + 1
-      if (i > line_start) then
-        temp_lines(nlines) = text(line_start:i-1)
-      else
-        temp_lines(nlines) = ""  ! Empty line
+      if (line_end >= line_start) then
+        temp_lines(nlines) = text(line_start:line_end)
       end if
-      line_start = i + 1
+      line_start = i
+    else if (text(i:i) == achar(10)) then  ! LF
+      line_end = i - 1
+      nlines = nlines + 1
+      if (line_end >= line_start) then
+        temp_lines(nlines) = text(line_start:line_end)
+      end if
+      i = i + 1
+      line_start = i
+    else
+      i = i + 1
     end if
   end do
-  
-  ! Handle last line (text after last newline, or entire text if no newlines)
-  nlines = nlines + 1
+
+  ! Handle last line if text doesn't end with newline
   if (line_start <= len(text)) then
+    nlines = nlines + 1
     temp_lines(nlines) = text(line_start:len(text))
-  else
-    temp_lines(nlines) = ""
   end if
-  
-  ! Allocate and copy to output
+
+  ! Copy to output
   allocate(character(len=max_line_len) :: lines(nlines))
   lines = temp_lines(1:nlines)
-  
+
 end subroutine split_lines
 
 
